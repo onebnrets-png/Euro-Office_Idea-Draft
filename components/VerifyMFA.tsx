3662937295df1deebd0c0e115935d52d12e0f014
Infinita <oneb.nrets@gmail.com>
2026-02-17 14:13:30 +0100

    Update VerifyMFA.tsx

--- components/VerifyMFA.tsx
+++ components/VerifyMFA.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { supabase } from '../services/storageService';
+import { supabase } from '../services/supabaseClient';
 
 export function VerifyMFA({ onVerified, language }) {
     const [verifyCode, setVerifyCode] = useState('');
